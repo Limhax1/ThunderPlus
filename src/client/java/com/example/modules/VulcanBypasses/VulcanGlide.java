5f8d7bf4f1cbf49a1571cb0104d3159de351ce92
Limhax <ballaarontest0@gmail.com>
2024-07-23 15:32:37 +0200

    fixed crash in the vulcan glide (oopsie)

--- src/client/java/com/example/modules/VulcanBypasses/VulcanGlide.java
+++ src/client/java/com/example/modules/VulcanBypasses/VulcanGlide.java
@@ -1,6 +1,7 @@
 package com.example.modules.VulcanBypasses;
 
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 import thunder.hack.events.impl.EventTick;
@@ -10,18 +11,19 @@ import thunder.hack.setting.Setting;
 public class VulcanGlide extends Module {
     public final Setting<Double> fallDistance = new Setting<Double>("Fall Distance", 1.0, 0.0, 5.0);
 
-    private double  fallDist = 0;
     public VulcanGlide() {
         super("VulcanGlide", Category.getCategory("VulcanBypasses"));
     }
 
     @EventHandler
-    private void onTick (EventTick event) {
-        Vec3d vel = mc.player.getVelocity();
-        fallDist = fallDistance.getValue();
+    private void onTick(EventTick event) {
+        ClientPlayerEntity player = mc.player;
+        Vec3d v = player.getVelocity();
+        if(player.fallDistance > fallDistance.getValue())
+            if(!player.isOnGround() && player.age % 2 == 0) {
+                player.setVelocity(v.x, -0.155, v.z);
+            };
 
-        if(mc.player.age % 0 == 2) {
-            mc.player.setVelocity(vel.x, -0.155, vel.z);
-        }
     }
 }
+
